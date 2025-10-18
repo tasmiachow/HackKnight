8f01fb590b0cc5a91da1567c98ae4d05358133c8
Ileana Aguilar <73076457+ileana-aguilar@users.noreply.github.com>
2025-10-18 09:56:48 -0400

    Added supabase auth, Navbar and some routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+
 import App from './App.jsx'
+import Login from './(auth)/Login.jsx'
+import Dashboard from './componets/Dashboard.jsx'
+import Protected from './(auth)/Protected.jsx'
+import SignUp from './(auth)/SignUp.jsx'
+import SignOut from './(auth)/SignOut.jsx'
+import HomeRedirect from './routes/HomeRedirect.jsx'
+import DashboardView from './routes/DashboardView.jsx'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<App/>}>
+        <Route index element={<HomeRedirect/>} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signout" element={<SignOut />} />
+        <Route
+          path="dashboard"
+          element={
+            <Protected>
+              <Dashboard />
+            </Protected>
+          }
+        />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
---
+++ client/src/routes/DashboardView.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../componets/Navbar';
+import Dashboard from '../componets/Dashboard';
+
+const DashboardView = () => {
+    return (
+        <div>
+            <Navbar />
+            <Dashboard/>
+            
+        </div>
+    );
+};
+
+export default DashboardView;
